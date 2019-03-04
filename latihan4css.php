6b3e21b62a0c71f0f4de9542e69f46eab79bb904
Eko Prasetyo <32763118+eko10@users.noreply.github.com>
2019-03-04 18:21:40 +0700

    Upload source code

---
+++ latihan4css.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>	Penggunaan Tag Link </title>
+</head>
+<body>
+
+	<h1>Belajar Tag Link</h1>
+	<p>Saya sedang belajar HTML dari
+		<a href="http://www.elematics-indonesia.com" target="_blank" style="background-color: #00FFFF;"> Elematics Indonesia</a> dan akan terbuka pada tab baru
+	</p>
+
+</body>
+</html>
---
+++ latihan61.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan 6.1</title>
+</head>
+<body style="background-color: #00008B;">
+
+	<p style="font-size: 18px; font-weight: bold; color: #FFFFFF;">Cara Membuat Website</p>
+
+</body>
+</html>
